431b5a732a8b90b58f3a8c2e86d7578f84736375
supriyanka-kadam <102313646+supriya-kawale@users.noreply.github.com>
2023-07-04 12:43:37 +0530

    Add files via upload

---
+++ login/page.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+function Login() {
+  return (
+    <div>
+        <h1>Login page</h1>s
+        <Link href="/">Go to Home page</Link>
+    </div>
+  )
+}
+
+export default Login
---
+++ page.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+function About() {
+  return (
+    <div>About
+    <h1>This is About page</h1>
+    <Link href="/">Go to about page</Link>
+    </div>
+  )
+}
+
+export default About
